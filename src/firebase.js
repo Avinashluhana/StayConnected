9998aa59ac8a54d92ff719859b1013113591f578
avinash_luhana <aviluhana@gmail.com>
2021-09-22 12:01:32 +0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC2d1ZdOxcwP6ExsI38UHwE9p1j0X0yYgE",
+    authDomain: "stay-connected-3bdfa.firebaseapp.com",
+    projectId: "stay-connected-3bdfa",
+    storageBucket: "stay-connected-3bdfa.appspot.com",
+    messagingSenderId: "929009676698",
+    appId: "1:929009676698:web:db46a7b3b9a6dbffb78a23",
+    measurementId: "G-MJKWEMSLCG"
+  };
